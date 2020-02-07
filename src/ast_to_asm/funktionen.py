5b30f3498f5d694cf542bed5c780289ffb5226ca
jan <jan.ph@local.int>
2020-02-07 18:45:53 +0100

    python zu python mit python

---
+++ src/ast_to_asm/funktionen.py
@@ -0,0 +1,94 @@
+class IF_COND:
+
+    # expr: Bedingung unter der der if-Block ausgeführt wird
+    # if_stat: Programmcode der ausgeführt wird wenn expr == True
+    # fl_stat: Der immer auf den if-Block folgende Code
+    def __init__(self, expr, if_stat, fl_stat):
+        self.expr = expr
+        self.if_stat = if_stat
+        self.fl_stat = fl_stat
+    
+    def ausgabe(self, v):
+        return (v)*" " + "IF\n" + self.expr.ausgabe(v+1) + "\n" + self.if_stat.ausgabe(v+2) + "\n" + self.fl_stat.ausgabe(v+1)
+
+    def generiere_asm(self, v):
+        return (v)*" " + "if " + self.expr.generiere_asm(v) + ":\n" + self.if_stat.generiere_asm(v+4) + "\n" + self.fl_stat.generiere_asm(v)
+
+class ASSIGN:
+
+    # id: Name der Variable
+    # expr: Wert der Variable (bisher nur integer)
+    # fl_stat: Die folgenden Statements
+    def __init__(self, id, expr, fl_stat):
+        self.id = id
+        self.expr = expr
+        self.fl_stat = fl_stat
+
+    def ausgabe(self, v):
+        return (v)*" " + "ASSIGN\n" + (v+1)*" " + self.id + "\n" + self.expr.ausgabe(v+2)+ "\n" + self.fl_stat.ausgabe(v+1)
+
+    def generiere_asm(self, v):
+        return (v)*" " + self.id + "=" + self.expr.ausgabe(0) + "\n" + self.fl_stat.generiere_asm(v)
+
+class WHILE:
+
+    # expr: Bedingung unter der der while-Block ausgeführt wird
+    # wh_stat: Programmcode der ausgeführt wird wenn expr == True
+    # fl_stat: Der immer auf den if-Block folgende Code 
+    def __init__(self, expr, wh_stat, fl_stat):
+        self.expr = expr
+        self.wh_stat = wh_stat
+        self.fl_stat = fl_stat
+
+    def ausgabe(self, v):
+        return (v) * " " + "WHILE\n" + self.expr.ausgabe(v + 1) + "\n" + self.wh_stat.ausgabe(
+            v + 2) + "\n" + self.fl_stat.ausgabe(v + 1)
+    
+    def generiere_asm(self, v):
+        return (v)*" " + "while " + self.expr.generiere_asm(v) + ":\n" + self.wh_stat.generiere_asm(v+4) + "\n" + self.fl_stat.generiere_asm(v)
+
+    
+class EXPR:
+
+    # expr: String
+    def __init__(self, lhand, op="",  rhand=""):
+        self.lhand = lhand
+        self.op = op
+        self.rhand = rhand
+    
+    def ausgabe(self, v):
+        return (v)*" " + self.lhand + self.op + self.rhand
+    
+    def generiere_asm(self, v):
+        return (v)*" " + self.lhand + self.op + self.rhand
+
+class FL_STAT:
+
+    def __init__(self, fl_stat):
+        self.fl_stat = fl_stat
+
+    
+    def ausgabe(self, v):
+        if self.fl_stat == "END":
+            return (v)*" " + self.fl_stat
+        else:
+            return self.fl_stat.ausgabe(v)
+    
+    def generiere_asm(self, v):
+        if self.fl_stat == "END":
+            return (v)*" " + "pass"
+        else:
+            return self.fl_stat.generiere_asm(v)
+
+class START:
+
+    def __init__(self, fl_stat):
+        self.fl_stat = fl_stat
+    
+    def ausgabe(self):
+        print(self.fl_stat.ausgabe(0))
+    
+    def generiere_asm(self):
+        return self.fl_stat.generiere_asm(0)
+    
+def optimus_prime(start):
---
+++ src/ast_to_asm/out.py
@@ -0,0 +1,7 @@
+if 3<3:
+    pass
+x=5+4
+while 3<4:
+    pass
+y=4
+pass
