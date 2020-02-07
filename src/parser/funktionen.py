bdb134e9a107dbc85e2847cc57f1c720245bfcce
Marc Thieme <zonico67@gmail.com>
2020-02-07 20:54:01 +0100

    Added EXPR blocks

--- src/parser/funktionen.py
+++ src/parser/funktionen.py
@@ -7,9 +7,11 @@ class IF_COND:
         self.expr = expr
         self.if_stat = if_stat
         self.fl_stat = fl_stat
-    
+
     def ausgabe(self, v):
-        return (v)*" " + "IF\n" + self.expr.ausgabe(v+1) + "\n" + self.if_stat.ausgabe(v+2) + "\n" + self.fl_stat.ausgabe(v+1)
+        return (v) * " " + "IF\n" + self.expr.ausgabe(v + 1) + "\n" + self.if_stat.ausgabe(
+            v + 2) + "\n" + self.fl_stat.ausgabe(v + 1)
+
 
 class ASSIGN:
 
@@ -22,13 +24,15 @@ class ASSIGN:
         self.fl_stat = fl_stat
 
     def ausgabe(self, v):
-        return (v)*" " + "ASSIGN\n" + (v+1)*" " + self.id + "\n" + self.expr.ausgabe(v+2)+ "\n" + self.fl_stat.ausgabe(v+1)
+        return (v) * " " + "ASSIGN\n" + (v + 1) * " " + self.id + "\n" + self.expr.ausgabe(
+            v + 2) + "\n" + self.fl_stat.ausgabe(v + 1)
+
 
 class WHILE:
 
     # expr: Bedingung unter der der while-Block ausgeführt wird
     # wh_stat: Programmcode der ausgeführt wird wenn expr == True
-    # fl_stat: Der immer auf den if-Block folgende Code 
+    # fl_stat: Der immer auf den if-Block folgende Code
     def __init__(self, expr, wh_stat, fl_stat):
         self.expr = expr
         self.wh_stat = wh_stat
@@ -37,32 +41,65 @@ class WHILE:
     def ausgabe(self, v):
         return (v) * " " + "WHILE\n" + self.expr.ausgabe(v + 1) + "\n" + self.wh_stat.ausgabe(
             v + 2) + "\n" + self.fl_stat.ausgabe(v + 1)
-    
+
+
 class EXPR:
 
     # expr: String
     def __init__(self, expr):
         self.expr = expr
-    
+
     def ausgabe(self, v):
-        return (v)*" " + self.expr
+        return (v) * " " + self.expr
+
 
 class FL_STAT:
 
     def __init__(self, fl_stat):
         self.fl_stat = fl_stat
 
-    
     def ausgabe(self, v):
         if self.fl_stat == "END":
-            return (v)*" " + self.fl_stat
+            return (v) * " " + self.fl_stat
         else:
             return self.fl_stat.ausgabe(v)
+
 
 class START:
 
     def __init__(self, fl_stat):
         self.fl_stat = fl_stat
-    
+
     def ausgabe(self):
         print(self.fl_stat.ausgabe(0))
+
+
+class EXPR0:
+    def __init__(self, e1, summand0=None):
+        self.e1 = e1
+        self.summand = summand
+
+
+class EXPR1:
+    def __init__(self, e2, factor1=None):
+        self.e2 = e2
+        self.factor = factor
+
+
+class EXPR2:
+    '''self.type = "neg" | "e3" '''
+
+    def __init__(self, type, negated2=None, e3=None):
+        self.type = type
+        self.negated2 = negated2
+        self.e3 = e3
+
+
+class EXPR3:
+    '''type = "ident" | "lit" | "e0" '''
+
+    def __init__(self, type, ident=None, lit=None, e0=None):
+        self.type = type
+        self.ident = ident
+        self.lit = lit
+        self.e0 = e0
