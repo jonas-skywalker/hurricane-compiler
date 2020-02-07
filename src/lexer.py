9424decef34e3c7baa5af6f60dfe5b7cdf03263f
Marc Thieme <zonico67@gmail.com>
2020-02-07 16:02:43 +0100

    Added tokenisation through 'tokenise_source'

---
+++ src/lexer.py
@@ -0,0 +1,63 @@
+def split_surround(source, c, label):
+    spl = source.split(c)
+    ret = []
+    for s in spl:
+        ret += [(s, None), (c, label)]
+    return ret[:-1]
+
+def split_symbols(source):
+    labels = [
+        (" ", "space"),
+        (";", "semicolon"),
+        ("{", "open_curly"),
+        ("}", "closed_curly"),
+        ("(", "open_bracket"),
+        (")", "closed_bracket"),
+        ("<=", "smaller_equals"),
+        (">=", "bigger_equals"),
+        ("==", "equals"),
+        ("=", "assign"),
+        ("+", "plus"),
+        ("-", "minus"),
+        ("*", "multi"),
+        ("/", "div")
+    ]
+    sources = [(source, None)]
+    for (c, label) in labels:
+        new_sources = []
+        for (split_word, matched_label) in sources:
+            if matched_label:
+                new_sources.append((split_word, matched_label))
+            else:
+                new_sources += split_surround(split_word, c, label)
+        sources = new_sources
+    return sources
+
+def match_keywords(tokens):
+    keywords = [
+        "if", "while", "print", "read", "int"
+    ]
+    for (index, (word, label)) in enumerate(tokens):
+        if not label and word in keywords:
+            tokens[index] = (word, word)
+        return tokens
+
+def match_idents_and_lits(tokens):
+    for (index, (word, label)) in enumerate(tokens):
+        if not label:
+            new_label = None
+            if word.isdigit():
+                new_label = "lit"
+            else:
+                new_label = "ident"
+            tokens[index] = (word, new_label)
+    return tokens
+
+def tokenise_source(source):
+    spl = split_symbols(source)
+    spl = match_keywords(spl)
+    spl = match_idents_and_lits(spl)
+    return spl
+
+def lex(source):
+    pass
